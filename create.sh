bc89382bfcd2f9395e163e336140634e5c4d3580
mikeplavsky <mike.plavsky@quest.com>
2016-09-13 08:23:14 +0000

    creating 256 sub-nets

--- create.sh
+++ create.sh
@@ -1,14 +1,20 @@
 CMD=$1
 NUM=$2
 
+function create_subnet {  
+    for ((i=1;i<=$NUM;i++)) do
+        docker network create --subnet=10.10.$i.0/24 test_$i
+    done
+}
+
 function create {  
-    for ((i=1;i<$NUM;i++)) do
+    for ((i=1;i<=$NUM;i++)) do
         docker network create test_$i
     done
 }
 
 function remove {  
-    for ((i=1;i<$NUM;i++)) do
+    for ((i=1;i<=$NUM;i++)) do
         docker network remove test_$i
     done
 }

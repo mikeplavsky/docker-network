36a693e29f957eb0bab8db8cd3bad0fc43602b8c
mikeplavsky <mike.plavsky@quest.com>
2016-09-13 09:03:04 +0000

    overlay network - can create 300 easily

--- create.sh
+++ create.sh
@@ -13,6 +13,12 @@ function create {
     done
 }
 
+function create_overlay {  
+    for ((i=1;i<=$NUM;i++)) do
+        docker network create --driver=overlay test_$i
+    done
+}
+
 function remove {  
     for ((i=1;i<=$NUM;i++)) do
         docker network remove test_$i
